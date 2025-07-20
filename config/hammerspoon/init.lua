ac2c8ea08e2cb91b1817fa499523b6356be2808c
krzysztofcybulski <krzysztofpcy@gmail.com>
2025-07-20 16:54:41 +0200

    Basic dotfiles

---
+++ config/hammerspoon/init.lua
@@ -0,0 +1,25 @@
+-- [[ Global Settings ]] ------------------------------------------------------
+Hyper = { "cmd", "alt" }
+
+hs.automaticallyCheckForUpdates(true)
+hs.menuIcon(true)
+hs.dockIcon(false)
+
+local function reload_config(files)
+  local doReload = false
+  for _, file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+
+-- Reload the config every time it changes
+hs.pathwatcher.new(os.getenv("HOME") .. "/.dotfiles/config/hammerspoon/", reload_config):start()
+
+-- [[ Modules ]] --------------------------------------------------------------
+require("apple-music-spotify-redirect")
+require("keymaps")
---
+++ config/hammerspoon/keymaps.lua
@@ -0,0 +1,53 @@
+
+local hyper = Hyper
+local host = require("hs.host")
+local name = host.localizedName()
+
+------------------------------- APP LAUNCH/TOGGLE ------------------------------
+--[[
+  The list of keys and apps which enable launching and toggling
+  Some apps have a different process name to their name on disk. To address
+  this, a table can be passed which contains the app name followed by the filename
+]]
+local apps = {
+  a = "Arc",
+  w = "WhatsApp",
+  s = "Slack",
+  f = "Figma",
+  m = "Spotify",
+  w = "Ghostty",
+}
+
+local LaunchOrToggle = function(key, app_name, app_filename)
+  hs.hotkey.bind(hyper, key, function()
+    local app = hs.application.find(app_name)
+    -- Toggle - show
+    local awin = nil
+    if app then
+      awin = app:mainWindow()
+    end
+    -- Toggle - hide
+    if awin and app and app:isFrontmost() then
+      app:hide()
+    else
+      -- Launch
+      if app_filename then
+        return hs.application.launchOrFocus(app_filename)
+      end
+
+      app = hs.application.find(app_name)
+
+      hs.application.launchOrFocus(app_name)
+      app.setFrontmost(app)
+      app.activate(app)
+    end
+  end)
+end
+
+for key, app_name in pairs(apps) do
+  if type(app_name) == "table" then
+    LaunchOrToggle(key, app_name[1], app_name[2])
+  else
+    LaunchOrToggle(key, app_name)
+  end
+end
